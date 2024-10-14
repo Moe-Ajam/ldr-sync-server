3041aea873409048a9fcde3924fefb2193a15711
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-10-14 23:38:05 +0300

    connected the project to a db with goose and sqlc; created the first migration for the users table; created the query to create a new user in the db

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Moe-Ajam/ldr-sync-server/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("CONN")
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("could not load database: %v\n", err)
+	}
+	dbQuries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQuries,
+	}
 	mux := http.NewServeMux()
 
 	srv := &http.Server{
