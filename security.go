c3026769325587cbf8292d418dd6b1fff77378f0
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-12-30 21:40:22 +0300

    Added the local extension id to the CORS allow list

--- security.go
+++ security.go
@@ -13,6 +13,7 @@ import (
 
 func enableCORS(w *http.ResponseWriter, r *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "chrome-extension://migncpjniacleljjjfdmkhpdomdkhjhn")
+	(*w).Header().Set("Access-Control-Allow-Origin", "chrome-extension://mkjhflenhpjedegkhgnjlconogccecmp")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
